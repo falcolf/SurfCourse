f49181e56e5a0f485707620cb913fc3855fdaca4
Dhruve Moudgil <dhruve.moudgildm@gmail.com>
2018-02-17 01:20:14 +0530

    check

--- CourseAggregator/app/src/main/java/com/apps/dcodertech/courseaggregator/MainActivity.java
+++ CourseAggregator/app/src/main/java/com/apps/dcodertech/courseaggregator/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
@@ -12,6 +13,12 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,23 +30,32 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+    private RequestQueue requestQueue;
+    private JsonObjectRequest objectRequest;
 
-    Button button;
-    EditText editText;
+    private Button button;
+    private EditText editText;
+    private String url="https://fscript.herokuapp.com/predict";
+
 
 
     @Override
@@ -48,104 +64,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
         editText = findViewById(R.id.et);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (TextUtils.isEmpty(editText.getText().toString())) {
                     Toast.makeText(MainActivity.this, "Enter a course", Toast.LENGTH_SHORT).show();
                 } else {
-                    String text = editText.getText().toString();
-                    AsyncTask asyncTask=new Asynctask();
-                    asyncTask.execute(text);
+                    String input=editText.getText().toString();
+                   dataFetch(input);
 
                 }
             }
         });
 
     }
-    public class Asynctask extends AsyncTask<String,Void,String>{
+    public void dataFetch(String input){
+        Map<String,String> map=new HashMap<>();
+        map.put("query",input);
 
-        @Override
-        protected String doInBackground(String... strings) {
-            String u="https://fscript.herokuapp.com/predict";
-            try {
-                URL url=new URL(u);
-                JSONObject jsonObject=new JSONObject();
-                jsonObject.put("query",strings[0]);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(15000 /* milliseconds */);
-                conn.setConnectTimeout(15000 /* milliseconds */);
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(getPostDataString(jsonObject));
-
-                writer.flush();
-                writer.close();
-                os.close();
-
-                int responseCode=conn.getResponseCode();
-
-                if (responseCode == HttpsURLConnection.HTTP_OK) {
-
-                    BufferedReader in=new BufferedReader(
-                            new InputStreamReader(
-                                    conn.getInputStream()));
-                    StringBuffer sb = new StringBuffer("");
-                    String line="";
-
-                    while((line = in.readLine()) != null) {
-
-                        sb.append(line);
-                        break;
-                    }
-
-                    in.close();
-                    return sb.toString();
-
-                }
-                else {
-                    return new String("false : "+responseCode);
-                }
+        requestQueue= Volley.newRequestQueue(this);
+        objectRequest=new JsonObjectRequest(Request.Method.POST, url, new JSONObject(map), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(getApplicationContext(),response.toString(),Toast.LENGTH_SHORT).show();
             }
-            catch(Exception e){
-                return new String("Exception: " + e.getMessage());
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
             }
-
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            Toast.makeText(MainActivity.this,s, Toast.LENGTH_SHORT).show();
-        }
+        });
+        requestQueue.add(objectRequest);
     }
-    public String getPostDataString(JSONObject params) throws Exception {
+    
 
-        StringBuilder result = new StringBuilder();
-        boolean first = true;
-
-        Iterator<String> itr = params.keys();
-
-        while(itr.hasNext()){
-
-            String key= itr.next();
-            Object value = params.get(key);
-
-            if (first)
-                first = false;
-            else
-                result.append("&");
-
-            result.append(URLEncoder.encode(key, "UTF-8"));
-            result.append("=");
-            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
-
-        }
-        return result.toString();
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
